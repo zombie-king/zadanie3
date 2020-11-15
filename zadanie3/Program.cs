885b074550b3f7c4bebabdb7705ac1da02b02c36
pgrabowski <pgrabowski@chathamfinancial.eu>
2020-11-15 22:39:38 +0100

    better error handling

--- zadanie3/Program.cs
+++ zadanie3/Program.cs
@@ -5,14 +5,24 @@ namespace zadanie3
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            int exitCode = 0;
             Parser.Default.ParseArguments<Arguments>(args)
                 .WithParsed(arguments =>
                 {
-                    var result = Rest.GiveTheRest(arguments.Rest);
-                    Console.WriteLine(Rest.FormatResult(result));
+                    try
+                    {
+                        var result = Rest.GiveTheRest(arguments.Rest);
+                        Console.WriteLine(Rest.FormatResult(result));
+                    }
+                    catch (ArgumentException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        exitCode = 1;
+                    }
                 });
+            return exitCode;
         }
     }
 }
